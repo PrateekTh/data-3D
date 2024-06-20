0cd1e57760b95c79e5f1181ca98e27c6d3d23181
PrateekTh <prateek33thakur@gmail.com>
2024-06-20 17:52:19 +0530

    Setup Basic Context

---
+++ Context-Theme/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useContext } from 'react';
+import UserContext from '../context/UserContext';
+
+function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {setUser} = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({username, password});
+    }
+    return(
+        <div>
+            <h2>Login</h2>
+            <input type='text' 
+            value={username} 
+            onChange={(e) => {setUsername(e.target.value)}} 
+            placeholder='Username'/> <br/>
+            
+            <input type='text' 
+            value={password} 
+            onChange={(e) => {setPassword(e.target.value)}} 
+            placeholder='Password'/> <br/>
+
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login;
